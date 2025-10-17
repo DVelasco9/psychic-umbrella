afa33c4593a58204593e83a1d0a4d1110d808c62
DVelasco9 <d12835571@gmail.com>
2025-10-17 08:37:43 -0700

    First Tkinter GUI

--- app.py
+++ app.py
@@ -15,13 +15,14 @@ root.geometry("500x500+500+100")
 #CREATE TWO LABELS
 label1 = tk.Label(root, text="Halloween is best holiday in the year")
 label1.pack()
-tk.Label(root, text="I LOVE U SO, I'L NEVER LET YOU GO...").pack()
+tk.Label(root, text="...").pack()
 
 #DISPLAY IMAGES
 image_path = r"c:\Users\Diego\Downloads\˚₊ᘛart creditᕀ🎐➧ @ AdeptaReality on x.jpg"
 original_image = Image.open(image_path)
 tk_image = ImageTk.PhotoImage(original_image)
 tk.Label(root, image=tk_image).pack()
+
 
 
 root.mainloop()
