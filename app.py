99896ed4cd14673a7e355b4c8d749040d184c8e5
DVelasco9 <d12835571@gmail.com>
2025-10-17 10:36:38 -0700

    Tkinter layouts

--- app.py
+++ app.py
@@ -4,28 +4,17 @@ from PIL import Image, ImageTk
 from tkinter import ttk
 
 root = tk.Tk()
+root.title("Frame demo")
+root.config(bg="skyblue")
 
-#SETTING SOME WINDOW PROPERTIES
-root.title("RK EXAMPLE")
-root.configure(background="white")
-root.minsize(200,200)
-root.maxsize(2000,2000)
-root.geometry("500x500+500+100")
-
-#CREATE TWO LABELS
-label1 = tk.Label(root, text="Halloween is best holiday in the year")
-label1.pack()
-tk.Label(root, text="...").pack()
+#CREATE A FRAME WIDGET
+frame = tk.Frame(root, width=200, height=200)
+frame.pack(padx=10, pady=10)
 
 #DISPLAY IMAGES
-image_path = r"c:\Users\Diego\Downloads\˚₊ᘛart creditᕀ🎐➧ @ AdeptaReality on x.jpg"
-original_image = Image.open(image_path)
-tk_image = ImageTk.PhotoImage(original_image)
-tk.Label(root, image=tk_image).pack()
-
-#CREATE AN EXIT BUTTON 
-#salida = ttk.Frame(root, padding=10)
-#salida.grid()
-#ttk.Button(salida, text="salida", command=root.destroy).grid(column=1, row=0)
+#image_path = r"c:\Users\Diego\Downloads\˚₊ᘛart creditᕀ🎐➧ @ AdeptaReality on x.jpg"
+#original_image = Image.open(image_path)
+#tk_image = ImageTk.PhotoImage(original_image)
+#tk.Label(root, image=tk_image).pack()
 
 root.mainloop()
